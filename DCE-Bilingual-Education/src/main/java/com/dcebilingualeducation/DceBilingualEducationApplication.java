50da367d4b03f2aadc247dced66b83f21b3270b8
EvalterTallyz <37478571+EvalterTallyz@users.noreply.github.com>
2019-10-31 14:18:04 -0300

    Criação do Projeto

---
+++ DCE-Bilingual-Education/src/main/java/com/dcebilingualeducation/DceBilingualEducationApplication.java
@@ -0,0 +1,13 @@
+package com.dcebilingualeducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DceBilingualEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DceBilingualEducationApplication.class, args);
+	}
+
+}
---
+++ DCE-Bilingual-Education/src/test/java/com/dcebilingualeducation/DCE/Bilingual/Education/DceBilingualEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcebilingualeducation.DCE.Bilingual.Education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DceBilingualEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
